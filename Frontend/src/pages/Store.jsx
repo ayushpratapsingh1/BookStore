832646a5e76373f8e074b8f4682b1f2c1c217033
Ayush Pratap Singh <ayushpratap3154@gmail.com>
2025-01-28 11:26:45 +0530

    Add Docker support with configuration files and deployment workflow; update Store component to use environment variable for API URL; enhance .gitignore for node_modules

--- Frontend/src/pages/Store.jsx
+++ Frontend/src/pages/Store.jsx
@@ -16,7 +16,7 @@ const Store = () => {
 
   const fetchBooks = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/books', {
+      const response = await fetch(import.meta.env.VITE_API_URL ||'http://localhost:5000/api/books', {
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
